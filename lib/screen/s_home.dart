a5ed9377cbb46997bf3b45dfaf17f4df01ae9ce7
ojyochan <yohan1101@oj.vision>
2024-08-06 14:47:51 +0900

    feat: movie data model 및 dummy 데이터 추가

--- lib/screen/s_home.dart
+++ lib/screen/s_home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../model/model_movie.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -8,6 +10,15 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<Movie> movies = [
+    Movie.fromMap({
+      'title': '사랑의 불시착',
+      'keyword': '사랑/로맨스/판타지',
+      'poster': 'test_movie1.png',
+      'like': false
+    }),
+  ];
+
   @override
   void initState() {
     super.initState();
